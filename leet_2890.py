d591008402495ecc033d5b33957611b14b9bf627
cmerkle1 <119691859+cmerkle1@users.noreply.github.com>
2026-03-04 13:08:41 -0500

    Create leet_2890.py

---
+++ leet_2890.py
@@ -0,0 +1,6 @@
+'''Reshape data so that each row represents
+sales data for a product in a specific quarter'''
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars=['product'], var_name='quarter', value_name='sales')
