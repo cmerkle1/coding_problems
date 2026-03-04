44f8a6f15775cb5d077646dc82e82caca5a3276f
cmerkle1 <119691859+cmerkle1@users.noreply.github.com>
2026-03-04 12:09:33 -0500

    Create leet_2886.py

---
+++ leet_2886.py
@@ -0,0 +1,6 @@
+'''Convert grade column in dataframe from float to int'''
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype('int64')
+    return students
